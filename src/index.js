0efc6bb90786bf26c09fcdd019a471f76a7d11cc
Andy Eccleston <eandy5000@yahoo.com>
2017-03-09 10:36:17 -0600

    added minimal header and footer components for basic build

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Header from './components/header';
+import Footer from './components/footer';
 
 const App = () => {
     return (
         <div>
             <Header />
+
+            <Footer />
         </div>
     );
 };

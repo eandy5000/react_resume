c86d3f36afa8f107f46bfac215a25991f867f9dc
Andy Eccleston <eandy5000@yahoo.com>
2017-03-09 11:12:49 -0600

    added store and root reducers

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
-import './styles/style.css'
+import './styles/style.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import reducers from './reducers';
 
 import Header from './components/header';
 import Footer from './components/footer';
 
-const App = () => {
+const App = (props) => {
     return (
         <div>
             <Header />
-
+            {props.children}
             <Footer />
         </div>
     );
 };
 
-ReactDOM.render(<App />,
+const store = createStore(reducers);
+
+ReactDOM.render(
+    <Provider store={store} >
+        <App />
+    </Provider>,
     document.querySelector('.target')
 );
